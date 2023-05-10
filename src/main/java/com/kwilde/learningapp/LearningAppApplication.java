5d5fd6d832db64ba6b82e2d4a6a41c0c9f02c9ba
keith-wilde <keithw453@gmail.com>
2023-05-09 21:13:30 -0400

    Initial Commit

---
+++ src/main/java/com/kwilde/learningapp/LearningAppApplication.java
@@ -0,0 +1,13 @@
+package com.kwilde.learningapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kwilde/learningapp/LearningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwilde.learningapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
