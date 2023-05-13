3a2fd56421e175ecb1a276dc82dcfcb2d05de762
keith-wilde <keithw453@gmail.com>
2023-05-13 13:01:08 -0400

    Created controller, added actuator, etc

---
+++ src/main/java/com/kwilde/learningapp/controller/PrimaryController.java
@@ -0,0 +1,14 @@
+package com.kwilde.learningapp.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrimaryController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> getHellowWorld(){
+        return ResponseEntity.ok("it workedsish");
+    }
+}
